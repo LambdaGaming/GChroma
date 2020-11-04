4c55cb433aa4a3f957c57048685786c86f9d661b
LambdaGaming <starwars18m@gmail.com>
2020-11-03 20:18:31 -0500

    Added function to control individual mouse lights

--- GChroma/main.cpp
+++ GChroma/main.cpp
@@ -9,7 +9,7 @@ using namespace GarrysMod::Lua;
 	GChroma_SetMouseColor( Vector color )
 	Arguments:
 		color - Lua RGB color table converted to a vector
-	Returns: boolean success
+	Returns: Boolean success
 	Example: GChroma_SetMouseColor( Vector( 0, 0, 255 ) )
 */
 LUA_FUNCTION( GChroma_SetMouseColor )
@@ -23,11 +23,52 @@ LUA_FUNCTION( GChroma_SetMouseColor )
 	{
 		COLORREF convert = RGB( color.x, color.y, color.z );
 		auto mouse = chromainit->SetMouseColor( convert );
+		delete chromainit;
 		LUA->PushBool( mouse );
 		return 1;
 	}
 	else
 	{
+		delete chromainit;
+		LUA->PushBool( false );
+		return 1;
+	}
+	delete chromainit;
+	LUA->PushBool( true );
+	return 1;
+}
+
+/*
+	GChroma_SetMouseColorEx( Vector color, Number row, Number col )
+	Arguments:
+		color - Lua RGB color table converted to a vector
+		row - LED profile row (https://developer.razer.com/works-with-chroma-v1/razer-chroma-led-profiles/)
+		col - LED profile column
+	Returns: Boolean success
+	Example: GChroma_SetMouseColorEx( Vector( 0, 0, 255 ), 2, 3 ) // Sets the scroll wheel color to blue
+*/
+LUA_FUNCTION( GChroma_SetMouseColorEx )
+{
+	GChroma* chromainit;
+	chromainit = new GChroma();
+	auto init = chromainit->Initialize();
+	LUA->CheckType( 1, GarrysMod::Lua::Type::Vector );
+	LUA->CheckType( 2, GarrysMod::Lua::Type::Number );
+	LUA->CheckType( 3, GarrysMod::Lua::Type::Number );
+	Vector color = LUA->GetVector( 1 );
+	double row = LUA->GetNumber( 2 );
+	double col = LUA->GetNumber( 3 );
+	if ( init )
+	{
+		COLORREF convert = RGB( color.x, color.y, color.z );
+		auto mouse = chromainit->SetMouseColorEx( convert, row, col );
+		delete chromainit;
+		LUA->PushBool( mouse );
+		return 1;
+	}
+	else
+	{
+		delete chromainit;
 		LUA->PushBool( false );
 		return 1;
 	}
@@ -39,7 +80,7 @@ LUA_FUNCTION( GChroma_SetMouseColor )
 /*
 	GChroma_ResetAll()
 	Arguments: None
-	Returns: boolean success
+	Returns: Boolean success
 */
 LUA_FUNCTION( GChroma_ResetAll )
 {
@@ -49,6 +90,7 @@ LUA_FUNCTION( GChroma_ResetAll )
 	if ( init )
 	{
 		chromainit->ResetEffects( ALL_DEVICES );
+		delete chromainit;
 		LUA->PushBool( true );
 		return 1;
 	}
@@ -62,6 +104,11 @@ GMOD_MODULE_OPEN()
 	LUA->PushSpecial( GarrysMod::Lua::SPECIAL_GLOB );
 		LUA->PushCFunction( GChroma_SetMouseColor );
 		LUA->SetField( -2, "GChroma_SetMouseColor" );
+	LUA->Pop();
+
+	LUA->PushSpecial( GarrysMod::Lua::SPECIAL_GLOB );
+		LUA->PushCFunction( GChroma_SetMouseColorEx );
+		LUA->SetField( -2, "GChroma_SetMouseColorEx" );
 	LUA->Pop();
 
 	LUA->PushSpecial( GarrysMod::Lua::SPECIAL_GLOB );
