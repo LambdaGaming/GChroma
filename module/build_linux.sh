a0882c92f110d11e1cb5ae6c5e62e73d6c4da408
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2026-04-13 11:20:49 -0400

    Finalize build scripts

--- module/build_linux.sh
+++ module/build_linux.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-rm CMakeCache.txt
-cmake ./ -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DNO_EXCEPTIONS=ON -DCMAKE_BUILD_TYPE=Release
+cmake ./ -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE=Release
 cmake --build . --clean-first
+
+# Rename file
+mv "gmsv_gchroma_linux64.dll" "gmcl_gchroma_linux64.dll"
+
+# Cleanup all the crap
+rm CMakeCache.txt
+rm -r CMakeFiles
+rm *.cmake
+rm Makefile
 echo "Finished"
--- module/build_windows.bat
+++ module/build_windows.bat
@@ -1,4 +1,24 @@
-del CMakeCache.txt
-cmake ./ -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DNO_EXCEPTIONS=ON
+@echo off
+rem Build dll
+cmake ./ -DCMAKE_POSITION_INDEPENDENT_CODE=ON
 cmake --build . --clean-first --config Release
+
+rem Move and rename binary
+cd Release
+ren "gmsv_gchroma_win64.dll" "gmcl_gchroma_win64.dll"
+copy "gmcl_gchroma_win64.dll" ".."
+cd ..
+
+rem Cleanup all the crap
+del /S *.sln
+del /S *.vcxproj*
+del /S *.cmake
+del CMakeCache.txt
+rmdir /s /Q CMakeFiles
+rmdir /s /Q Release
+rmdir /s /Q x64
+rmdir /s /Q ALL_BUILD.dir
+rmdir /s /Q INSTALL.dir
+rmdir /s /Q gchroma.dir
+rmdir /s /Q ZERO_CHECK.dir
 @echo Finished
